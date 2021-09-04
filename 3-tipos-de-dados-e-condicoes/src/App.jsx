4f5a01aa0712128693a3037fca419d5f969afa3e
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-09-04 16:30:14 -0300

    Dados e condições - Manipulando eventos

--- 3-tipos-de-dados-e-condicoes/src/App.jsx
+++ 3-tipos-de-dados-e-condicoes/src/App.jsx
@@ -1,56 +1,27 @@
 import React from "react";
 
-const listCustomer = [
-    {
-        id: 1,
-        nome: 'Alessandra Luisi',
-        skills: ['JS', 'HTML', 'CSS', 'React', 'Node']
-    },
-    {
-        id: 2,
-        nome: 'Bruno Carneiro',
-        skills: ['React', 'Node', 'CSS', 'Webpack']
-    },
-    {
-        id: 3,
-        nome: 'Aline Gomes',
-        skills: ['Kotlin']
-    },
-    {
-        id: 4,
-        nome: 'Greg Tolentino',
-        skills: ['Comer', 'Dormir', 'Roncar']
-    }
-]
-
 const App = () => {
 
-    const renderSkills = (skill, index) => {
-        return (
-            <div style={{ paddingLeft: '30px' }} key={`skill-${index}`}>
-                <li>{skill}</li>
-            </div>
-        )
-    }
+    const name = 'Digital Innovation One';
 
-    const renderCustomers = (customer, index) => {
-        return (
-            <div key={/*index*/`customer-${customer.id}`}>
-                <li>{customer.nome}</li>
-                {customer.skills.map(renderSkills)}
-            </div>
-        )
+    const showEvent = (event) => {
+        console.log('evento clicado');
+        console.log(event);
+        alert(name);
+    }
+    const Button = <button onClick={showEvent}>Mostrar Evento</button>
+
+    const handleChange = (event) => {
+        const { value } = event.target;
+        console.log(value);
     }
 
     return (
         <div>
             <p>Didital Innovation One</p>
             <p>Bem vindo a nossa aula =D</p>
-            <div>
-                <ul>
-                    {listCustomer.map(renderCustomers)}
-                </ul>
-            </div>
+            <input onChange={handleChange} />
+            {Button}
         </div>
     )
 };
